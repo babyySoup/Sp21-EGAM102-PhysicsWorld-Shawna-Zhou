2e095a14d8687f73da33c43f64d9da43e3ca27fe
babyySoup <shawnazy02@gmail.com>
2021-02-10 00:15:54 -0800

    push

---
+++ Fishi/Assets/Script/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementForce;
+
+
+
+private void FixedUpdate()
+{
+    if(Input.GetKey(KeyCode.D))
+    {
+        Rigidbody2D thisrigidbody2D;
+        thisrigidbody2D = GetComponent<Rigidbody2D>();
+        thisrigidbody2D.AddForce(MovementForce * Vector3.right, ForceMode2D.Force);
+    }
+
+    if (Input.GetKey(KeyCode.A))
+    {
+        Rigidbody2D thisrigidbody2D;
+        thisrigidbody2D = GetComponent<Rigidbody2D>();
+        thisrigidbody2D.AddForce(MovementForce * Vector3.left, ForceMode2D.Force);
+    }
+}
+}
