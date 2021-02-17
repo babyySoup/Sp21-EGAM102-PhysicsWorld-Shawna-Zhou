ef63f17ce8f2d0f14cc2d2e0262d735d3464f10f
babyySoup <71286763+babyySoup@users.noreply.github.com>
2021-02-16 16:34:17 -0800

    rocket show

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    //set the variables that we are going to use for the code
     public Vector3 RocketForce;
     public float RocketDuration;
 
     public float ExplosionDelay;
     public int NExplosionPieces;
     public float ExplosionForce;
+    //call the prefab
     public GameObject ExplosionPiecePrefab;
 
+    //set interface
     private float RocketCutoffTime;
     private float ExplosionTime;
 
@@ -30,10 +33,11 @@ public class Rocket : MonoBehaviour
             GetComponent<HomebrewRigidbody2D>().TotalForce += RocketForce;
         //check over and over where if it reached the explosion time
         if(Time.time > ExplosionTime)
-        {
+        {//if yes then the rocket explode
             GameObject newExplosionPiece; 
             for (int i = 0; i < NExplosionPieces; i++)
             {
+                //transform the positions using Rigidbody2D
                 newExplosionPiece = Instantiate(ExplosionPiecePrefab);
                 newExplosionPiece.transform.position = this.transform.position;
                 newExplosionPiece.GetComponent<HomebrewRigidbody2D>().Velocity = this.GetComponent<HomebrewRigidbody2D>().Velocity;
